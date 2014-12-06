92d3184fd1784c0994e7eb5aac16d419e92ed13f
Francis Newson <fon@hep.ph.bham.ac.uk>
2014-12-06 17:11:25 +0100

    More dch scattering modelling

--- apps/src/toy_scatter.cpp
+++ apps/src/toy_scatter.cpp
@@ -3,10 +3,52 @@
 #include "HistStore.hh"
 #include "TRandom3.h"
 #include "Counter.hh"
+#include <memory>
+
+std::unique_ptr<TF1> quadratic_scattering( std::string name, double magnitude, double peak )
+{
+    //Function tends to magnitude/x^2
+    //Function peaks at peak
+
+    std::unique_ptr<TF1> result{ new TF1
+        ( name.c_str(), "[0]/x**2 * tanh( pow( x*[1], 4 ) )", -5*peak, 5*peak )  };
+
+
+    //Magic number to get peak in the right place
+    double xscale = 1.021;
+
+    double p0 = magnitude;
+    double p1 = xscale / peak;
+
+    result->SetParameter( 0, p0 );
+    result->SetParameter( 1, p1 );
+
+    return result;
+}
+
+std::unique_ptr<TF1> quartic_scattering( std::string name, double magnitude, double peak )
+{
+    //Function tends to magnitude/x^2
+    //Function peaks at peak
+
+    std::unique_ptr<TF1> result{ new TF1
+        ( name.c_str(), "[0]/x**4 * tanh( pow( x*[1], 6 ) )", -5*peak, 5*peak )  };
+
+
+    //Magic number to get peak in the right place
+    double xscale = 0.965;
+
+    double p0 = magnitude;
+    double p1 = xscale / peak;
+
+    result->SetParameter( 0, p0 );
+    result->SetParameter( 1, p1 );
+
+    return result;
+}
 
 int main( int argc, char * argv[] )
 {
-
     //Set up program options
     po::options_description general("general");
 
@@ -50,9 +92,10 @@ int main( int argc, char * argv[] )
     double sigma = 1.5e-4;
     TF1 fgaus("fgaus", "[0]*exp(-0.5*((x-[1])/[2])**2)", -0.005, 0.005 );
     fgaus.SetNpx( 2000 );
-    fgaus.SetParameters( 100, 0, sigma );
+    fgaus.SetParameters( 1, 0, sigma );
 
-    TF1 ftail("ftail", "tanh( pow(x/[0], 6 ) ) * pow(x,-4) ", -0.005, 0.005 );
+    //TF1 ftail("ftail", "tanh( pow(x/[0], 6 ) ) * pow(x,-4) ", -0.005, 0.005 );
+    TF1 ftail("ftail", "tanh( pow(x/[0], 4 ) ) * pow(x,-2) ", -0.005, 0.005 );
     ftail.SetNpx( 2000 );
     ftail.SetParameter( 0, 3.85*sigma );
 
@@ -62,7 +105,7 @@ int main( int argc, char * argv[] )
 
     for ( int i = 0; i != 10; ++i )
     {
-        double ratio = 0.01 * i ;
+        double ratio = 0.2 * i * 0.009;
         rates.push_back( ratio );
         hcombos.push_back( hs.MakeTH1D( Form( "hcombo_%d", i ), Form( "COMBO, %f", ratio ),
                     1000, -0.01, 0.01, "x" ) );
@@ -97,7 +140,7 @@ int main( int argc, char * argv[] )
 
         for (  int i = 0 ; i != 10 ; ++i )
         {
-        //std::cout<< "Shift" << std::endl;
+            //std::cout<< "Shift" << std::endl;
             double shift = ftail.GetRandom();
             htails[i]->Fill( shift );
 
